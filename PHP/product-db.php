ee7a9e8ae90be67bf23e892ceaca9386d31fc30c
Miguel <22-09056@g.batstate-u.edu.ph>
2024-12-08 14:03:02 +0800

    products page minor improvements (clickable product cards)

--- PHP/product-db.php
+++ PHP/product-db.php
@@ -3,9 +3,8 @@ if ($prodres->num_rows > 0) {
                         while($row = $prodres->fetch_assoc()) {
                             echo "<div id='product-card'>";
                             echo "<img src='" . htmlspecialchars($row['image']) . "' alt='" . htmlspecialchars($row['productName']) . "' class='product-image'>";
-                            echo "<button id='product-button'>" . htmlspecialchars($row['productName']) . "</button>";
+                            echo "<h2>" . htmlspecialchars($row['productName']) . "</h2>"; 
                             echo "<p>Price: $" . $row['price'] . "</p>";
-                            echo "<p>" .$row['prodDescription'] . "</p>";
                             echo "</div>";
                         }
                     
